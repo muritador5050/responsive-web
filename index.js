8c8c867bfa48ff50ba700c35322571c58b481eff
muritador5050 <muritador5050@gmail.com>
2025-02-14 23:43:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('.nav__item');
+const modal = document.getElementById('modal');
+elements.forEach((element) => {
+  element.addEventListener('click', () => (modal.style.display = 'block'));
+});
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
